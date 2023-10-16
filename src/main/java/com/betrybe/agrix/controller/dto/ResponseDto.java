9a98e4fef09db5c5c43942a1919042e581644751
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 16:44:31 -0300

    fix: desativa temporariamente Response Dto

--- src/main/java/com/betrybe/agrix/controller/dto/ResponseDto.java
+++ src/main/java/com/betrybe/agrix/controller/dto/ResponseDto.java
@@ -1,9 +1,4 @@
 package com.betrybe.agrix.controller.dto;
 
-/**
- * The type Response dto.
- *
- * @param <T> the type parameter
- */
-public record ResponseDto<T>(T data) {
-}
+//public record ResponseDto<T>(T) {
+//}
