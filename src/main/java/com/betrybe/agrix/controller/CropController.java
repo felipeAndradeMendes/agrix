57d8e59776a5e0bca5ace6d3a690e56dfdf67085
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 19:20:56 -0300

    feat: implementa metodo getCropById na CropController

--- src/main/java/com/betrybe/agrix/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/controller/CropController.java
@@ -1,12 +1,14 @@
 package com.betrybe.agrix.controller;
 
 import com.betrybe.agrix.controller.dto.CropDto;
+import com.betrybe.agrix.models.entities.Crop;
 import com.betrybe.agrix.services.FarmService;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +48,23 @@ public class CropController {
         .collect(Collectors.toList());
 
     return ResponseEntity.ok(allCrops);
+  }
+
+  /**
+   * Gets crop by id.
+   *
+   * @param id the id
+   * @return the crop by id
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<CropDto> getCropById(@PathVariable Integer id) {
+    Crop foundCrop = farmService.getCropById(id);
+    CropDto foundCropDto = new CropDto(
+        foundCrop.getId(),
+        foundCrop.getName(),
+        foundCrop.getPlantedArea(),
+        foundCrop.getFarm().getId()
+    );
+    return ResponseEntity.ok(foundCropDto);
   }
 }
