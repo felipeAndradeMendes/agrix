f9451a9b5073cf5a1fd7054a083cd76f900302dc
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 16:30:48 -0300

    feat: implementa metodo getFarmById na service

--- src/main/java/com/betrybe/agrix/services/FarmService.java
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -1,8 +1,10 @@
 package com.betrybe.agrix.services;
 
+import com.betrybe.agrix.exception.FarmNotFoundException;
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.models.repositories.FarmRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,7 +36,28 @@ public class FarmService {
     return farmRepository.save(farm);
   }
 
+  /**
+   * Gets all farms.
+   *
+   * @return the all farms
+   */
   public List<Farm> getAllFarms() {
     return farmRepository.findAll();
+  }
+
+  /**
+   * Gets farm by id.
+   *
+   * @param id the id
+   * @return the farm by id
+   */
+  public Farm getFarmById(Integer id) {
+    Optional<Farm> optionalFarm = farmRepository.findById(id);
+
+    if (optionalFarm.isEmpty()) {
+      throw new FarmNotFoundException();
+    }
+
+    return optionalFarm.get();
   }
 }
