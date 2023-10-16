7822932897abda35944f8d0277c4d5c2748f69e3
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 15:17:29 -0300

    feat: implementa FarmService e metodo insertFarm

---
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -0,0 +1,22 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.models.repositories.FarmRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FarmService {
+
+  private FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  public Farm insertFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+}
