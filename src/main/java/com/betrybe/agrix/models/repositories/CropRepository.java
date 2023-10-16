5fe1e3a4d005cdbdb4fe346de51d5c44b6141d62
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 19:18:47 -0300

    fix: anota corretamente repositorio Crop e corrige paramentro da interface

--- src/main/java/com/betrybe/agrix/models/repositories/CropRepository.java
+++ src/main/java/com/betrybe/agrix/models/repositories/CropRepository.java
@@ -2,9 +2,11 @@ package com.betrybe.agrix.models.repositories;
 
 import com.betrybe.agrix.models.entities.Crop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * The interface Crop repository.
  */
-public interface CropRepository extends JpaRepository<Crop, InternalError> {
+@Repository
+public interface CropRepository extends JpaRepository<Crop, Integer> {
 }
