3537948c2baa66e04a9980f35910ee2f8cff5602
Felipe Mendes <felipemadruga@gmail.com>
2023-10-16 16:07:56 -0300

    feat: implementa metodo getAllFarms na service

--- src/main/java/com/betrybe/agrix/services/FarmService.java
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -2,9 +2,12 @@ package com.betrybe.agrix.services;
 
 import com.betrybe.agrix.models.entities.Farm;
 import com.betrybe.agrix.models.repositories.FarmRepository;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * The type Farm service.
@@ -32,5 +35,10 @@ public class FarmService {
    */
   public Farm insertFarm(Farm farm) {
     return farmRepository.save(farm);
+  }
+
+  @GetMapping
+  public List<Farm> getAllFarms() {
+    return farmRepository.findAll();
   }
 }
