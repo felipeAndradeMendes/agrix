723d351ac237d6829e7016c85abcaa34d45fae53
Felipe Mendes <felipemadruga@gmail.com>
2023-10-25 18:02:32 -0300

    feat<req 10>: implementa handler para FertilizerNotFoundException

--- src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
+++ src/main/java/com/betrybe/agrix/advice/GeneralControllerAdvice.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.advice;
 
 import com.betrybe.agrix.exception.CropNotFoundException;
 import com.betrybe.agrix.exception.FarmNotFoundException;
+import com.betrybe.agrix.exception.FertilizerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -30,6 +31,14 @@ public class GeneralControllerAdvice {
   @ExceptionHandler(CropNotFoundException.class)
   public ResponseEntity<String> handleCropNotFoundException(
       CropNotFoundException exception
+  ) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(exception.getMessage());
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<String> handleFertilizerNotFoundException(
+      FertilizerNotFoundException exception
   ) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
         .body(exception.getMessage());
