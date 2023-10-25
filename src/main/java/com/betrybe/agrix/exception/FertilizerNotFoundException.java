1cc7e7adad914ea1114967c945182d0b534f5fc6
Felipe Mendes <felipemadruga@gmail.com>
2023-10-25 17:54:24 -0300

    feat<req 10>: implementa FertilizerNotFoundException.

---
+++ src/main/java/com/betrybe/agrix/exception/FertilizerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.betrybe.agrix.exception;
+
+public class FertilizerNotFoundException extends RuntimeException{
+  public FertilizerNotFoundException() {
+    super("Fertilizante não encontrado!");
+  }
+}
