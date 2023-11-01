49f889bfe3b345bdccd253ad646c3da54cbfc088
Felipe Mendes <felipemadruga@gmail.com>
2023-11-01 15:16:38 -0300

    feat<req 02>: cria PersonController com metodo insertPerson

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/PersonController.java
@@ -0,0 +1,50 @@
+package com.betrybe.agrix.ebytr.staff.controller;
+
+import com.betrybe.agrix.ebytr.staff.controller.dto.PersonDto;
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Person controller.
+ */
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+  private final PersonService personService;
+
+  /**
+   * Instantiates a new Person controller.
+   *
+   * @param personService the person service
+   */
+  @Autowired
+  public PersonController(PersonService personService) {
+    this.personService = personService;
+  }
+
+  /**
+   * Insert person response entity.
+   *
+   * @param person the person
+   * @return the response entity
+   */
+  @PostMapping
+  public ResponseEntity<PersonDto> insertPerson(@RequestBody Person person) {
+    Person personSaved = personService.create(person);
+    PersonDto personDto = new PersonDto(
+        personSaved.getId(),
+        person.getUsername(),
+        personSaved.getRole()
+    );
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(personDto);
+  }
+}
